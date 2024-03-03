f73052a58c57966cfa8f17c8706188d0b63b873f
Your Name <tahsin.mail.0503@gmail.com>
2024-03-04 05:34:03 +0600

    all section complited

--- scripts/app.js
+++ scripts/app.js
@@ -7,47 +7,58 @@ const loadData = async () => {
 const fatchData = (items) => {
     console.log(items);
     items.forEach(item => {
+        const bgClass = item.isActive === true ? 'bg-[red]' : 'bg-[#10B981]';       
         console.log(item.title)
         const itemContainer = document.getElementById('itemContainer');
         const div = document.createElement('div');
-        div.classList = 'showSearchItem card w-[95%] lg:w-[100%] bg-[#F7F8F8] shadow-xl p-1 ';
+        div.classList = 'showSearchItem card w-[95%] lg:w-[100%] bg-[#F7F8F8] shadow-xl p-1 mx-auto';
         div.innerHTML = `
         <div class="card-body grid grid-cols-7 gap-0">
         <div class="w-10 lg:w-16 h-16  col-span-1 relative">
-            <img class="rounded-lg" src="${item.image}" alt="">
-            <button id="showItem1" class=""></button>
+             <img class="rounded-lg" src="${item.image}" alt="">
+             <button class="showItemButton  ${bgClass} w-4 h-4 rounded-full absolute -top-1 lg:-top-1.5 left-8 lg:left-14 "></button>
+
         </div>
-        <div class="col-span-6">
-            <div class="grid grid-cols-10 text-[--p-color] text-sm font-medium">
-                <p class="col-span-2">#${item.category}</p>
-                <p class="col-span-8">Author : ${item.author.name}</p>
-            </div>
-            <div>
-                <h1 class="text-xl font-bold text-[#12132D] mb-5 mt-1" >${item.title}</h1>
-                <p class="font-normal text-[--p-color] text-sm ">${item.description}</p>
-            </div>
-            <hr class=" border-dashed my-5 bg-[--p-color] ">
-            <div class="grid grid-cols-2">
+         <div class="col-span-6">
+             <div class="grid grid-cols-10 text-[--p-color] text-sm font-medium">
+                 <p class="col-span-2">#${item.category}</p>
+                 <p class="col-span-8">Author : ${item.author.name}</p>
+             </div>
+             <div>
+                 <h1 class="text-xl font-bold text-[#12132D] mb-5 mt-1" >${item.title}</h1>
+                 <p class="font-normal text-[--p-color] text-sm ">${item.description}</p>
+             </div>
+             <hr class=" border-dashed my-5 bg-[--p-color] ">
+             <div class="grid grid-cols-2">
             <div class="flex justify-center gap-5 ">
-                <p><i class="fa-solid fa-message"></i> ${item.comment_count}</p>
+                 <p><i class="fa-solid fa-message"></i> ${item.comment_count}</p>
                 <p><i class="fa-regular fa-eye"></i> ${item.view_count}</p>
-                <p><i class="fa-regular fa-clock"></i> ${item.posted_time} min</p>
+                 <p><i class="fa-regular fa-clock"></i> ${item.posted_time} min</p>
+             </div>
+             <div class="flex justify-end mr-10">
+             <button id="showItem" onclick="showItemBtn('${item.title.replace(/'/g, 'title')}', '${item.view_count}')" class="btn btn-accent rounded-full py-0"><i class="fa-regular fa-envelope-open"></i></button>
             </div>
-            <div class="flex justify-end mr-10">
-            <div>
-            <button  onclick="showItemBtn('${item.title.replace(/'/g, 'title')}', '${item.view_count}')" class="btn btn-accent rounded-full py-0"><i class="fa-regular fa-envelope-open"></i></button>
-
-
-        </div>
-
-        </div>
+         </div>
         </div>
 `;
-        itemContainer.appendChild(div)
-       
-        
+itemContainer.appendChild(div)     
+
+           
     });
+
+
+
 }
+
+
+
+ 
+
+
+
+
+
+
 let count=0;
 
 const showItemBtn = (title, view) => {
@@ -91,7 +102,7 @@ const fatchLatestData = (allNews) => {
         console.log(news.title)
         const latestNewsContainer = document.getElementById('latestNewsContainer');
         const div = document.createElement('div');
-        div.classList = ' bg-[#F7F8F8] card card-compact w-[90%] bg-base-100 shadow-xl';
+        div.classList = ' bg-[#F7F8F8] card card-compact w-[95%] bg-base-100 shadow-xl mx-auto';
         div.innerHTML = `
         <figure><img src="${news.cover_image}" alt="Shoes" />
                 </figure>
@@ -146,13 +157,15 @@ const searchDataFatch = (searchItems) => {
     console.log(searchItems);
     searchItems.posts.forEach(item => {
         console.log(item)
-
+        const bgClass = item.isActive === true ? 'bg-[red]' : 'bg-[#10B981]';  
         const div = document.createElement('div');
         div.classList = 'showSearchItem card w-[95%] lg:w-[100%] bg-[#F7F8F8] shadow-xl p-1 ';
         div.innerHTML = `
        <div class="card-body grid grid-cols-7 gap-0">
-        <div class="w-10 lg:w-16 h-16  col-span-1 ">
+        <div class="w-10 lg:w-16 h-16  col-span-1 relative">
              <img class="rounded-lg" src="${item.image}" alt="">
+             <button class="showItemButton  ${bgClass} w-4 h-4 rounded-full absolute -top-1.5 left-14 "></button>
+
         </div>
          <div class="col-span-6">
              <div class="grid grid-cols-10 text-[--p-color] text-sm font-medium">
